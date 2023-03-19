2a40325695a1dd7c1ce406d2c12e6f2d708da19c
Brian Shu <littlebubu.shu@gmail.com>
2023-03-18 21:36:26 -0400

    add simulate

--- src/Text/HLex/Internal/Dfa/Simulate.hs
+++ src/Text/HLex/Internal/Dfa/Simulate.hs
@@ -1,7 +1,21 @@
 module Text.HLex.Internal.Dfa.Simulate where
 
+import Control.Applicative ((<|>))
 import Data.ByteString (ByteString)
-import Text.HLex.Internal.Dfa (Dfa)
+import Data.ByteString qualified as B
+import Data.IntMap qualified as IntMap
+import Data.Vector.Persistent qualified as PVec
+import Data.Word (Word8)
+import Text.HLex.Internal.Dfa (Dfa, Dfa' (Dfa))
+import Text.HLex.Internal.Dfa qualified as Dfa
 
-simulate :: ByteString -> Dfa a -> Maybe a
-simulate = undefined
+simulate :: ByteString -> Dfa a -> Maybe (Int, a)
+simulate bs Dfa {Dfa.start, Dfa.states} = go start 0 Nothing
+  where
+    go s i lastMatch = case B.indexMaybe bs i of
+      Nothing -> (i,) <$> Dfa.accept (PVec.index s states)
+      Just b -> do
+        let state = PVec.index s states
+        case IntMap.lookup (fromIntegral @Word8 @Int b) (Dfa.transitions state) of
+          Nothing -> (i,) <$> lastMatch
+          Just s' -> go s' (i + 1) (Dfa.accept state <|> lastMatch)
